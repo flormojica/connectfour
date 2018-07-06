d7cc43879174ee7c5681a0b9582e593c5928048a
flormojica <flormojica00@gmail.com>
2018-07-06 10:03:25 -0400

    adding

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-let currentPlayer = "red";
+let board = new Array(6);
+for (let i = 0; i < 7; i++) board[i] = new Array();
+console.log(board);
+let currentPlayer = 'yellow';
 
-const columns = document.querySelectorAll(".column");
+let col = document.querySelectorAll('.column');
+for (let i = 0; i < col.length; i++)
+	col[i].addEventListener('click', function (event) {
+            console.log(event.target.parentElement.querySelectorAll('.row'));
+		for (let cell of Array.from(event.target.parentElement.querySelectorAll('.row')))
+			if (cell.innerHTML === '') {
+				console.log('found at ', cell);
+				let checker = document.createElement('div');
+				checker.classList.add('checker', currentPlayer);
+                cell.appendChild(checker);
+                board[i].push(currentPlayer === 'yellow' ? '0' : '1');
+                console.log(board[i]);
 
-const players={
-    true: "red",
-    false:"blue"
-}
-for(let i=0; i < columns.length; i++){
-    const column= columns[i];
-    column.onclick = function () {
-        if(currentPlayer == "red"){
-            currentPlayer = "blue";
-        }
-        else{
-            currentPlayer="red"
-        }
-    const checker = document.createElement("div");
-    checker.classList.add("checker",currentPlayer);
-    column.appendChild(checker);
-    redTurn = players[!currentPlayer];
+				currentPlayer = currentPlayer === 'yellow' ? 'red' : 'yellow';
+				break;
+			}
+	});
 
-console.log(currentPlayer);
-}
-}
